a7372eefa40460fc4f37ec1ebdd43da8ebe855e7
OscarZant <santillanoscarariel@gmail.com>
2026-02-09 18:26:42 -0600

    Added a password section for user validation :)

--- helloGit.py
+++ helloGit.py
@@ -4,16 +4,43 @@ import sys
 
 class HelloGit:
 
-    def __init__(self,userName):
+    def __init__(self,userName, userPassword):
 
         self.userName = userName
+        self.userPassword = userPassword
     
     def helloUser(self):
 
         return f"Hello {self.userName}"
 
+def userPassword():
+
+    while True:
+
+        try:
+
+            userPassword = int(input("Enter a numerical password: "))
+
+            if (len(str(userPassword))) != 6:
+
+                print("Invalid password, try again")
+            
+            else:
+
+                print("Valid Password")
+                break
+            
+        except ValueError:
+            
+            print(f"{userPassword}is not valid")
+        
+        return userPassword
+    
+password = userPassword()
+
+
 if __name__ == "__main__":
 
-    hiG = HelloGit("Oscar")
+    hiG = HelloGit("Oscar",password)
     hiG.helloUser()
 
